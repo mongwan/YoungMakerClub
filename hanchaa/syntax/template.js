b663d3de8234c97e602e9efc1ecc312c3285a622
hanchaa <hanchaa@gmail.com>
2019-05-29 12:17:56 +0000

    study

---
+++ hanchaa/syntax/template.js
@@ -0,0 +1,7 @@
+var name = 'egoing';
+var letter = 'dfjakb ' + name + '\nvj;kdasjf;k ' + name + ' ladjskvjkadjsvbk;jda;kfj';
+
+var letter = `dfjakb ${1 + 1}
+vj;kdasjf;k ${name} ladjskvjkadjsvbk;jda;kfj`;
+console.log(letter);
+
---
+++ hanchaa/syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'egoing';
+var letter = 'dfjakb ' + name + ' vj;kdasjf;k ' + name + ' ladjskvjkadjsvbk;jda;kfj';
+console.log(letter);
